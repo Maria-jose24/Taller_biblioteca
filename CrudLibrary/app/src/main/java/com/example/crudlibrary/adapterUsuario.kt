b1363391f8e802cd9b604e3e3398d98be1849398
unknown <mariajosemurciamartinez85@gmail.com>
2024-08-04 14:21:47 -0500

    Se actualiza

--- CrudLibrary/app/src/main/java/com/example/crudlibrary/adapterUsuario.kt
+++ CrudLibrary/app/src/main/java/com/example/crudlibrary/adapterUsuario.kt
@@ -14,39 +14,38 @@ import org.json.JSONObject
 class adapterUsuario (
     var listaUsuario: JSONArray,
     var context: Context
-    ): RecyclerView.Adapter<adapterUsuario.MyHolder>() {
+): RecyclerView.Adapter<adapterUsuario.MyHolder>() {
 
-        inner class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val lblnombre_completo: TextView = itemView.findViewById(R.id.lblnombre_completo)
-            val spinner_tipo_usuario: TextView = itemView.findViewById(R.id.spinner_tipo_usuario)
-            val btnEditar: ImageView = itemView.findViewById(R.id.btnEditar)
-            val btnEliminar: ImageView = itemView.findViewById(R.id.btnEliminar)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
-            val itemView = LayoutInflater.from(context).inflate(R.layout.item_usuario, parent, false)
-            return MyHolder(itemView)
-        }
-
-        override fun onBindViewHolder(holder: MyHolder, position: Int) {
-            val usuario = listaUsuario.getJSONObject(position)
-            holder.lblnombre_completo.text = usuario.getString("nombre_completo")
-            holder.spinner_tipo_usuario.text = usuario.getString("tipo_usuario")
-
-            holder.btnEditar.setOnClickListener {
-                onclickEditar?.invoke(usuario)
-            }
-
-
-            holder.btnEliminar.setOnClickListener {
-                onclickEliminar?.invoke(usuario)
-            }
-        }
-
-        override fun getItemCount(): Int {
-            return listaUsuario.length()
-        }
-
-        var onclickEditar: ((JSONObject) -> Unit)? = null
-        var onclickEliminar: ((JSONObject) -> Unit)? = null
+    inner class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val lblnombre_completo: TextView = itemView.findViewById(R.id.lblnombre_completo)
+        val spinner_tipo_usuario: TextView = itemView.findViewById(R.id.spinner_tipo_usuario)
+        val btnEditar: ImageView = itemView.findViewById(R.id.btnEditar)
+        val btnEliminar: ImageView = itemView.findViewById(R.id.btnEliminar)
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.item_usuario, parent, false)
+        return MyHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        val usuario = listaUsuario.getJSONObject(position)
+        holder.lblnombre_completo.text = usuario.getString("nombre_completo")
+        holder.spinner_tipo_usuario.text = usuario.getString("tipo_usuario")
+
+        holder.btnEditar.setOnClickListener {
+            onclickEditar?.invoke(usuario)
+        }
+
+        holder.btnEliminar.setOnClickListener {
+            onclickEliminar?.invoke(usuario)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listaUsuario.length()
+    }
+
+    var onclickEditar: ((JSONObject) -> Unit)? = null
+    var onclickEliminar: ((JSONObject) -> Unit)? = null
+}
--- CrudLibrary/app/src/main/java/com/example/crudlibrary/detalleUsuarioFragment.kt
+++ CrudLibrary/app/src/main/java/com/example/crudlibrary/detalleUsuarioFragment.kt
@@ -7,68 +7,92 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.crudlibrary.config.config
 import com.example.crudlibrary.models.usuario
 import com.google.gson.Gson
 import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-private var id_usuario: String? =null
-
-
-/**
- * A simple [Fragment] subclass.
- * Use the [detalleUsuarioFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class detalleUsuarioFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            id_usuario = it.getString("id_usuario").toString()
-        }
-    }
+    private var id_usuario: String? = null
 
-    private lateinit var lblnombre_completo: TextView
-    private lateinit var lbldireccion: TextView
-    private lateinit var lblcorreo: TextView
-    private lateinit var spinnerTipoUsuario: Spinner
+    // Definir las variables
+    private lateinit var lblnombre_completo: EditText
+    private lateinit var lbldireccion: EditText
+    private lateinit var lblcorreo: EditText
+    private lateinit var lbl_tipo_usuario: Spinner
     private lateinit var btnEditar: Button
     private lateinit var btnEliminar: Button
     private lateinit var btnVolver: ImageView
 
-    //Se asigna un id existente temporal
-    private var id_usuario: String = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            id_usuario = it.getString("id_usuario")
+        }
+    }
 
-    fun consultarUsuario() {
-        id_usuario?.let{
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflar el layout para este fragmento
+        val view = inflater.inflate(R.layout.fragment_detalle_usuario, container, false)
+        // Inicializar las vistas
+        lblnombre_completo = view.findViewById(R.id.lblnombre_completo)
+        lbldireccion = view.findViewById(R.id.lbldireccion)
+        lblcorreo = view.findViewById(R.id.lblcorreo)
+        lbl_tipo_usuario = view.findViewById(R.id.lbl_tipo_usuario)
+        btnEditar = view.findViewById(R.id.btnEditar)
+        btnEliminar = view.findViewById(R.id.btnEliminar)
+        btnVolver = view.findViewById(R.id.imageView2)
+
+        // Configurar el Spinner
+        val opciones = arrayOf("Seleccionar...", "Lector", "Bibliotecario", "Administrador")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, opciones)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        lbl_tipo_usuario.adapter = adapter
+
+        // Configurar los botones
+        btnEditar.setOnClickListener { editarUsuario() }
+        btnEliminar.setOnClickListener { id_usuario?.let { eliminarUsuario(it) } }
+        btnVolver.setOnClickListener {
+            // Regresa al fragmento de la lista de usuarios
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        // Consultar el usuario si el ID no es nulo
+        id_usuario?.let { consultarUsuario() }
+
+        return view
+    }
+
+    private fun consultarUsuario() {
+        id_usuario?.let {
             val request = JsonObjectRequest(
                 Request.Method.GET,
-                config.urlUsuarios + it,
-                null,//parametros
+                "${config.urlUsuarios}$it",
+                null,
                 { response ->
                     val gson = Gson()
                     val usuario = gson.fromJson(response.toString(), usuario::class.java)
-                    lblnombre_completo.text = usuario.nombre_completo
-                    lbldireccion.text = usuario.direccion
-                    lblcorreo.text = usuario.correo
-                    val adapter = spinnerTipoUsuario.adapter as ArrayAdapter<String>
-                    spinnerTipoUsuario.setSelection(adapter.getPosition(usuario.tipo_usuario))
+                    lblnombre_completo.setText(usuario.nombre_completo)
+                    lbldireccion.setText(usuario.direccion)
+                    lblcorreo.setText(usuario.correo)
+                    val opciones = arrayOf("Seleccionar...", "Lector", "Bibliotecario", "Administrador")
+                    val tipoUsuarioIndex = opciones.indexOf(usuario.tipo_usuario)
+                    if (tipoUsuarioIndex >= 0) {
+                        lbl_tipo_usuario.setSelection(tipoUsuarioIndex)
+                    }
                     Toast.makeText(context, "Se consultó correctamente", Toast.LENGTH_LONG).show()
                 },
                 { error ->
@@ -80,90 +104,54 @@ class detalleUsuarioFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_detalle_usuario, container, false)
-        lblnombre_completo = view.findViewById(R.id.lblnombre_completo)
-        lbldireccion = view.findViewById(R.id.lbldireccion)
-        lblcorreo = view.findViewById(R.id.lblcorreo)
-        spinnerTipoUsuario = view.findViewById(R.id.spinner_tipo_usuario)
-        btnEditar = view.findViewById(R.id.btnEditar)
-        btnEditar.setOnClickListener { editarUsuario() }
-        btnEliminar = view.findViewById(R.id.btnEliminar)
-        btnEliminar.setOnClickListener { id_usuario?.let { eliminarUsuario(it) } }
-        btnVolver = view.findViewById(R.id.imageView2)
-
-        btnVolver.setOnClickListener {
-            requireActivity().supportFragmentManager.popBackStack()
-        }
-        id_usuario.let {
-            consultarUsuario()
-        }
-        return view
-    }
-
-    fun editarUsuario() {
+    private fun editarUsuario() {
         id_usuario?.let {
-            val parametros = JSONObject()
-            parametros.put("nombre_completo", lblnombre_completo.text.toString())
-            parametros.put("direccion", lbldireccion.text.toString())
-            parametros.put("correo", lblcorreo.text.toString())
-            parametros.put("tipo_usuario", spinnerTipoUsuario.selectedItem.toString())
+            val parametros = JSONObject().apply {
+                put("nombre_completo", lblnombre_completo.text.toString())
+                put("direccion", lbldireccion.text.toString())
+                put("correo", lblcorreo.text.toString())
+                put("tipo_usuario", lbl_tipo_usuario.selectedItem.toString())
+            }
 
             val request = JsonObjectRequest(
-                Request.Method.PUT, //metodo de la peticion
-                config.urlUsuarios + it, //url
-                parametros, //parametros
+                Request.Method.PUT,
+                "${config.urlUsuarios}$it",
+                parametros,
                 { response ->
-                    Toast.makeText(context, response.getString("respuesta"), Toast.LENGTH_LONG)
-                        .show()
+                    Toast.makeText(context, "Se actualizó correctamente", Toast.LENGTH_LONG).show()
+                    parentFragmentManager.popBackStack()
                 },
                 { error ->
                     Toast.makeText(context, "Error al actualizar", Toast.LENGTH_LONG).show()
-                }//error en la petición
-            )
-            val queue = Volley.newRequestQueue(context)
-            queue.add(request)
-        }
-    }
-    //Se crea el metodo eliminar usuario
-
-    fun eliminarUsuario(id_usuario: String) {
-            val request = JsonObjectRequest(
-                Request.Method.DELETE, // Método DELETE para eliminar el recurso
-                config.urlUsuarios + id_usuario, // URL del recurso con el ID del usuario a eliminar
-                null, // No se envían parámetros en el cuerpo para DELETE
-                { response ->
-                    Toast.makeText(context, "Usuario eliminado correctamente", Toast.LENGTH_LONG)
-                        .show()
-                    // Aquí puedes implementar lógica adicional después de eliminar el libro
-                },
-                { error ->
-                    Toast.makeText(context, "Error al eliminar el usuario", Toast.LENGTH_LONG)
-                        .show()
                 }
             )
             val queue = Volley.newRequestQueue(context)
             queue.add(request)
+        }
+    }
+
+    private fun eliminarUsuario(id_usuario: String) {
+        val request = StringRequest(
+            Request.Method.DELETE,
+            "${config.urlUsuarios}$id_usuario",
+            { response ->
+                Toast.makeText(context, "Usuario eliminado correctamente", Toast.LENGTH_LONG).show()
+                parentFragmentManager.popBackStack()
+            },
+            { error ->
+                Toast.makeText(context, "Error al eliminar el usuario", Toast.LENGTH_LONG).show()
+            }
+        )
+        val queue = Volley.newRequestQueue(context)
+        queue.add(request)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment detalleUsuarioFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(id_usuario: String) =
             detalleUsuarioFragment().apply {
                 arguments = Bundle().apply {
-                   putString("id_usuario", id_usuario)
+                    putString("id_usuario", id_usuario)
                 }
             }
     }
